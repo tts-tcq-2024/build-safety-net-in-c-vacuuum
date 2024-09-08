7a0ba1ec045d98377384325590f4fb40aca691dc
vacuuum <47915224+vacuuum@users.noreply.github.com>
2024-09-08 23:22:45 +0530

    Test cases update

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,12 +1,73 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
 
+//tests for single character and for 'F' [CASE 1]
 TEST(SoundexTestsuite, ReplacesConsonantsWithAppropriateDigits_1)
 {
-    //tests for zero padding and soundex for 'X' [CASE 2]
+
+    char soundex[5];
+    const char *answer = "F000";
+    generateSoundex("F", soundex);
+    for(int i =0; i<5;i++)
+    {
+        ASSERT_EQ(answer[i],soundex[i]);
+    }
+}
+
+//tests zero padding and soundex for 'X' [CASE 2]
+TEST(SoundexTestsuite, ReplacesConsonantsWithAppropriateDigits_2)
+{
     char soundex[5];
     const char *answer = "A200";
     generateSoundex("AX", soundex);
+    for(int i =0; i<5;i++)
+    {
+        ASSERT_EQ(answer[i],soundex[i]);
+    }
+}
+
+//tests if the function drops occurances of vowels for 'D' [CASE 3]
+TEST(SoundexTestsuite, ReplacesConsonantsWithAppropriateDigits_3)
+{
+    char soundex[5];
+    const char *answer = "D000";
+    generateSoundex("DIOU", soundex);
+    for(int i =0; i<5;i++)
+    {
+        ASSERT_EQ(answer[i],soundex[i]);
+    }
+}
+
+//tests for when 2 adjacent letters encode to the same number 'M' and 'N' [CASE 4,5]
+TEST(SoundexTestsuite, ReplacesConsonantsWithAppropriateDigits_4)
+{
+    char soundex[5];
+    const char *answer = "L500";
+    generateSoundex("LMNO", soundex);
+    for(int i =0; i<5;i++)
+    {
+        ASSERT_EQ(answer[i],soundex[i]);
+    }
+}
+
+//tests for when 2 adjacent letters encode to the same number and is separated by a vowel
+TEST(SoundexTestsuite, ReplacesConsonantsWithAppropriateDigits_5)
+{
+    char soundex[5];
+    const char *answer = "P141";
+    generateSoundex("PAVLOV", soundex);
+    for(int i =0; i<5;i++)
+    {
+        ASSERT_EQ(answer[i],soundex[i]);
+    }
+}
+
+//tests for when 2 letters with the same no are separated by hw
+TEST(SoundexTestsuite, ReplacesConsonantsWithAppropriateDigits_6)
+{
+    char soundex[5];
+    const char *answer = "H300";
+    generateSoundex("HDTW", soundex);
     for(int i =0; i<5;i++)
     {
         ASSERT_EQ(answer[i],soundex[i]);
